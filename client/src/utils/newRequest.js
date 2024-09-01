8a66889d33e2e17ada330d73a9a2b424259d20e1
snehadurai <snehatamilachi@gmail.com>
2024-09-01 22:26:16 +0530

    forntendurl

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL || "http://localhost:3000",
+  baseURL: import.meta.env.VITE_BASE_URL || "https://quizee2.vercel.app",
   withCredentials: true,
 });
 
