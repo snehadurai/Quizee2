37c1f9b7cea8619d3b20ef47efded2a0df35db86
snehadurai <snehatamilachi@gmail.com>
2024-09-01 22:52:33 +0530

    add back url

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,10 +1,15 @@
+// import axios from "axios";
 
-// newRequest.js
+// export const newRequest = axios.create({
+//   baseURL: import.meta.env.VITE_BASE_URL || "http://localhost:3000",
+//   withCredentials: true,
+// });
+
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL || "https://quizee2.vercel.app",
+  baseURL: import.meta.env.VITE_BASE_URL || 'https://quizee2.vercel.app',
   withCredentials: true,
 });
 
-export default newRequest; // Ensure default export is present
+export default newRequest;
